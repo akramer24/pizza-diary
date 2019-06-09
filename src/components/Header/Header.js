70fdab3c5f3db7e200375630eb2b218aee84b151
Ari Kramer <arikramer@Aris-MBP.fios-router.home>
2019-06-09 13:11:30 -0400

    add less and create Firebase and Header components

---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div id="header">
+      <span id="header-title">Pizza Diary</span>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/components/index.js
@@ -0,0 +1,2 @@
+export * from './Firebase';
+export { default as Header } from './Header/Header';
